8dd93511061557c169f11d849cd562ff9620f4e9
Mathur <mathurk29@gmail.com>
2022-04-17 16:29:27 +0530

    BST search

---
+++ BST search.py
@@ -0,0 +1,17 @@
+# https: // www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+
+def search(root, key):
+
+    # Base Cases: root is null or key is present at root
+    if root is None or root.val == key:
+        return root
+
+    # Key is greater than root's key
+    if root.val < key:
+        return search(root.right, key)
+
+    # Key is smaller than root's key
+    return search(root.left, key)
+
+
+# This code is contributed by Bhavya Jain
