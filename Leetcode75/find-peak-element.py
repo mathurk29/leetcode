49196d574f9c7d81d9a11216dc17130128cc2f83
Kshitij Mathur <Kshitij.Mathur@cantire.com>
2024-06-25 11:38:24 +0530

    common mistakes

--- Leetcode75/find-peak-element.py
+++ Leetcode75/find-peak-element.py
@@ -5,10 +5,21 @@ class Solution:
 
     def findPeakElement(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
+
+        # if n == 1:
+        #     return 0
+
+        # below is better
+
+        if nums[0] > nums[1]:
             return 0
-        left_pointer = 0
-        right_pointer = n - 1
+        if nums[n-1] > nums[n-2]: # n-1 is last element stupid - not n.
+            return n
+
+        # search remaining array now
+        left_pointer = 1
+        right_pointer = n - 2
+
         while left_pointer <= right_pointer: # use <= in binary search
             mid_pointer = (left_pointer + right_pointer) // 2 # calculate mid_pointer inside while
             if self.isPeak(nums, mid_pointer, n):
@@ -19,10 +30,12 @@ class Solution:
                 right_pointer = mid_pointer - 1
 
     def isPeak(self, nums, location, n):
-        if location == 0:
-            return nums[location] > nums[location + 1]
-        if location == n-1:
-            return nums[location - 1] < nums[location]
+        ## NOT required following as special quick cases are identified and handled at the start of prog.
+
+        # if location == 0:
+        #     return nums[location] > nums[location + 1]
+        # if location == n-1:
+        #     return nums[location - 1] < nums[location]
         return (
             nums[location - 1] < nums[location] and nums[location + 1] < nums[location] # middle element has to be greater than neigbor - NOT _ < _ < _
         )
