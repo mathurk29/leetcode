72b56d2b62e47bfd80cf3101d5b4f798dc493306
Kshitij Mathur <Kshitij.Mathur@cantire.com>
2024-06-25 11:30:52 +0530

    black

--- Leetcode75/longest-subarray-of-1s-after-deleting-one-element.py
+++ Leetcode75/longest-subarray-of-1s-after-deleting-one-element.py
@@ -36,19 +36,20 @@ class Solution:
         """
 
         n = len(nums)
-        if sum(nums) == n: return n - 1 # all are `1`
+        if sum(nums) == n:
+            return n - 1  # all are `1`
 
         dp = [[0] * 2 for _ in range(len(nums))]
         dp[0][0] = nums[0]
 
-        for i in range(1,n):
+        for i in range(1, n):
             pass
             if nums[i] == 1:
                 dp[i][0] = dp[i - 1][0] + 1
                 dp[i][1] = dp[i - 1][1] + 1
             if nums[i] == 0:
                 dp[i][0] = 0
-                dp[i][1] = dp[i-1][0]
+                dp[i][1] = dp[i - 1][0]
 
         return max([i for j in dp for i in j])
 
